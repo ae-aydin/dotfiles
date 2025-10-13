831f7c633798fe8a450a3d19a999b00abf3600b5
ae-aydin <aydnarifenes@gmail.com>
2025-10-13 17:03:25 +0300

    Update installation script, fix path resolving

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOTFILES_DIR="$(pwd)"
+DOTFILES_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$HOME/.config"
 
 link() {
@@ -22,18 +22,15 @@ echo
 
 link "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 link "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-link "$DOTFILES_DIR/.config/dunst" "$HOME/.config/dunst"
+# link "$DOTFILES_DIR/.config/dunst" "$HOME/.config/dunst"
+link "$DOTFILES_DIR/.config/fish" "$HOME/.config/fish"
 link "$DOTFILES_DIR/.config/fastfetch" "$HOME/.config/fastfetch"
 link "$DOTFILES_DIR/.config/foot" "$HOME/.config/foot"
 link "$DOTFILES_DIR/.config/fuzzel" "$HOME/.config/fuzzel"
 link "$DOTFILES_DIR/.config/ghostty" "$HOME/.config/ghostty"
 link "$DOTFILES_DIR/.config/hypr" "$HOME/.config/hypr"
 link "$DOTFILES_DIR/.config/kitty" "$HOME/.config/kitty"
-link "$DOTFILES_DIR/.config/rofi" "$HOME/.config/rofi"
-
-mkdir -p "$HOME/.config/spicetify/Themes"
-link "$DOTFILES_DIR/.config/spicetify/Themes/text-everforest" "$HOME/.config/spicetify/Themes/text-everforest"
-
+link "$DOTFILES_DIR/.config/mako" "$HOME/.config/mako"
 link "$DOTFILES_DIR/.config/waybar" "$HOME/.config/waybar"
 link "$DOTFILES_DIR/.config/yazi" "$HOME/.config/yazi"
 link "$DOTFILES_DIR/.config/zed" "$HOME/.config/zed"
